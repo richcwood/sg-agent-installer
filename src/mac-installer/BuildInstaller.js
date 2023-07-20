fb6393c1439bbefd7bfc10766ddf6bf18a3105c4
Richard Wood <rich@saasglue.com>
2023-07-19 22:25:05 -0600

    update nodejs version

--- src/mac-installer/BuildInstaller.js
+++ src/mac-installer/BuildInstaller.js
@@ -2,4 +2,4 @@ const pkg_1 = require("pkg");
 
 const build = process.argv[2];
 
-pkg_1.exec([`install_sg_agent_mac.js`, '--config', `package.json`, '--targets', 'node10-macos', '--out-path', `./build/${build}`]);
+pkg_1.exec([`install_sg_agent_mac.js`, '--config', `package.json`, '--targets', 'node18-macos', '--out-path', `./build/${build}`]);
--- src/mac-installer/BuildInstaller_arm64.js
+++ src/mac-installer/BuildInstaller_arm64.js
@@ -2,4 +2,4 @@ const pkg_1 = require("pkg");
 
 const build = process.argv[2];
 
-pkg_1.exec([`install_sg_agent_mac.js`, '--config', `package.json`, '--targets', 'node10-macos-arm64', '--out-path', `./build/${build}`]);
+pkg_1.exec([`install_sg_agent_mac.js`, '--config', `package.json`, '--targets', 'node18-macos-arm64', '--out-path', `./build/${build}`]);
